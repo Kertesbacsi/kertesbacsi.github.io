f3261e37f2521746b52fc31ce1841dcd0dd6cc26
Kertesbacsi <122312208+Kertesbacsi@users.noreply.github.com>
2023-01-09 21:02:03 +0100

    Add files via upload

---
+++ responsive2.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
+	
+}
+
+
+var className = "inverted";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add(className);
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove(className);
+  }
+};
