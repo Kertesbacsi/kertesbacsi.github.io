71c6e0320e26f63d98b853ea48098d54e8fee7a8
Kertesbacsi <122312208+Kertesbacsi@users.noreply.github.com>
2023-01-15 18:14:58 +0100

    Update responsive2.js

--- responsive2.js
+++ responsive2.js
@@ -23,6 +23,9 @@ window.onscroll = function() {
 };
 
 
+
+
+
 const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0],
 arrowIcons = document.querySelectorAll(".wrapper i");
@@ -61,38 +64,103 @@ const autoSlide = () => {
     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
 }
 
-const dragStart = (e) => {
-    // updatating global variables value on mouse down event
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
+
+
+
+
+const carousel2 = document.querySelector(".carousel2"),
+igen = carousel2.querySelectorAll("img")[0],
+nyilak = document.querySelectorAll(".wrapper2 nav");
+
+let ishuz = false, huz2ging = false, kövi, balra, különbség;
+
+const Ikonok = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel2.scrollWidth - carousel2.clientWidth; // getting max scrollable width
+    nyilak[0].style.display = carousel2.scrollLeft == 0 ? "none" : "block";
+    nyilak[1].style.display = carousel2.scrollLeft == scrollWidth ? "none" : "block";
 }
 
-const dragging = (e) => {
-    // scrolling images/carousel to left according to mouse pointer
-    if(!isDragStart) return;
-    e.preventDefault();
-    isDragging = true;
-    carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    showHideIcons();
+nyilak.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let igenWidth = igen.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel2.scrollLeft += icon.id == "left" ? -igenWidth : igenWidth;
+        setTimeout(() => Ikonok(), 60); // calling Ikonok after 60ms
+    });
+});
+
+const automata = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel2.scrollLeft - (carousel2.scrollWidth - carousel2.clientWidth) > -1 || carousel2.scrollLeft <= 0) return;
+
+    különbség = Math.abs(különbség); // making különbség value to positive
+    let igenWidth = igen.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = igenWidth - különbség;
+
+    if(carousel2.scrollLeft > balra) { // if user is scrolling to the right
+        return carousel2.scrollLeft += különbség > igenWidth / 3 ? valDifference : -különbség;
+    }
+    // if user is scrolling to the left
+    carousel2.scrollLeft -= különbség > igenWidth / 3 ? valDifference : -különbség;
 }
 
-const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
 
-    if(!isDragging) return;
-    isDragging = false;
-    autoSlide();
+
+const carousel3 = document.querySelector(".carousel3"),
+igen33 = carousel3.querySelectorAll("img")[0],
+nyilak3 = document.querySelectorAll(".wrapper3 section");
+
+let ishuz33 = false, huz32ging = false, kövi33, balra33, különbésg33;
+
+const ikonok3 = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel3.scrollWidth - carousel3.clientWidth; // getting max scrollable width
+    nyilak[0].style.display = carousel3.scrollLeft == 0 ? "none" : "block";
+    nyilak[1].style.display = carousel3.scrollLeft == scrollWidth ? "none" : "block";
 }
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
+nyilak3.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let igen3Width = igen33.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel3.scrollLeft += icon.id == "left" ? -igen3Width : igen3Width;
+        setTimeout(() => ikonok3(), 60); // calling ikonok3 after 60ms
+    });
+});
 
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
+const automata3 = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel3.scrollLeft - (carousel3.scrollWidth - carousel3.clientWidth) > -1 || carousel3.scrollLeft <= 0) return;
 
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);  
+    különbésg3 = Math.abs(különbésg3); // making különbésg3 value to positive
+    let igen3Width = igen3.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = igen3Width - különbésg3;
+
+    if(carousel3.scrollLeft > balra3) { // if user is scrolling to the right
+        return carousel3.scrollLeft += különbésg3 > igen3Width / 3 ? valDifference : -különbésg3;
+    }
+    // if user is scrolling to the left
+    carousel3.scrollLeft -= különbésg3 > igen3Width / 3 ? valDifference : -különbésg3;
+}
+
+
+// Get the modal
+   var modal = document.getElementById("myModal");
+   // Get the image and insert it inside the modal - use its "alt" text as a caption
+   var img = document.getElementById("myImg");
+   var modalImg = document.getElementById("img01");
+   var captionText = document.getElementById("caption");
+   img.onclick = function() {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+   };
+   // Get the <span> element that closes the modal
+   var span = document.getElementsByClassName("close")[0];
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() {
+      modal.style.display = "none";
+   };
