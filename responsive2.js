f889409575c45b0212ca5b40acf554aae7be7cd1
Kertesbacsi <122312208+Kertesbacsi@users.noreply.github.com>
2023-01-16 17:02:27 +0100

    Update responsive2.js

--- responsive2.js
+++ responsive2.js
@@ -58,7 +58,7 @@ const autoSlide = () => {
     let valDifference = firstImgWidth - positionDiff;
 
     if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 1 ? valDifference : -positionDiff;
     }
     // if user is scrolling to the left
     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
@@ -164,3 +164,33 @@ const automata3 = () => {
    span.onclick = function() {
       modal.style.display = "none";
    };
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
